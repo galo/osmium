c32cfb6c8536935d2c3fcc4734d96cedf5941837
Galo <galo.gimenez@gmail.com>
2012-02-12 22:04:52 +0100

    just created

---
+++ osmium/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Osmium::Application.config.secret_token = 'b2c7e46b92cef20745dfad7d39892ccf438d5821660479ab3ed15b333cbb91d4a3aebf4dda3049f41a7f3ed27ab9224d49ffbba46bc4c7b68d276356340e359d'
